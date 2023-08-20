6627add2a74bbe9db65b26d9a37d0bf37820d4a9
Artem Priadchenko <yorgan.info@gmail.com>
2023-08-20 16:57:40 +0300

    ыва

---
+++ Task_5/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_5.Models;
+
+namespace Task_5.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Item> ListIt = new List<Item>()
+                 {
+                new Item(1,"Телефон", 2467),
+                new Item(2,"Камера", 5678),
+                new Item(3,"Штатив", 245),
+                new Item(4,"Кран", 8948),
+                new Item(5,"Микшер", 2324),
+
+                 };
+
+            ViewBag.ItemCollection = ListIt;
+
+            return View();
+        }
+
+
+        public IActionResult Index2()
+        {
+
+            List<Item> ListIt = new List<Item>()
+                 {
+                new Item(1,"Телефон", 2467),
+                new Item(2,"Камера", 5678),
+                new Item(3,"Штатив", 245),
+                new Item(4,"Кран", 8948),
+                new Item(5,"Микшер", 2324),
+
+                 };
+
+            return View(ListIt);
+        }
+
+    }
+}
+
+
